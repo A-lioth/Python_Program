82f89d02b0bf6d863ed202f6b8542ae0ef8cad42
Alioth <3434039767@qq.com>
2024-10-05 15:36:08 +0800

    ✨ feat: 提交了 type() 和 数据类型转换测试文件。

---
+++ Python-3days/数据类型/test.py
@@ -0,0 +1,12 @@
+print(type("Hello World"))
+print(type(123))
+print(type(3.14))
+print(type(True))
+print(type(None))
+num = 1
+int(num)
+print(type(num))
+
+# * 字符串转数字要求字符串中只能包含数字
+str = int("你好")
+print(type(str))
