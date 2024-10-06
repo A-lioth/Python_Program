35a54dc947ee79dd8056a9230f36fb3271313a4e
Alioth <3434039767@qq.com>
2024-10-06 16:47:29 +0800

    ✨ fix: 修正了九九乘法表的输出格式，使其更易读。

--- Python-3days/Practice/九九乘法表/test.py
+++ Python-3days/Practice/九九乘法表/test.py
@@ -1,7 +1,6 @@
 
 # * 九九乘法表
 for i in range(1, 10):
-    for j in range(1, 10):
-        if j <= i:
-            print(f"{j}*{i}={j*i}", end="\t")
+    for j in range(1, i + 1):
+        print(f"{j} * {i} = {j * i}", end="\t")
     print()
