87aed6c25498c9e3e7fa559b5b2c780ff9f7cbac
Alioth <3434039767@qq.com>
2024-10-07 20:52:46 +0800

    ✨ feat: 提交了使用字典对员工的工资及级别调整的测试文件。

---
+++ Python-3days/Practice/dict/test.py
@@ -0,0 +1,39 @@
+employees = {
+    "wang": {
+        "部门": "科技",
+        "工资": 3000,
+        "级别": 1
+        },
+    "zhou": {
+        "部门": "市场",
+        "工资": 5000, 
+        "级别": 2
+        },
+    "lin": {
+        "部门": "市场", 
+        "工资": 7000, 
+        "级别": 3
+        },
+    "zhang": {
+        "部门": "科技",
+        "工资": 4000, 
+        "级别": 1
+        },
+    "liu": {
+        "部门": "市场",
+        "工资": 6000, 
+        "级别": 2
+        }
+}
+print(employees)
+
+for name in employees:
+    if employees[name]["级别"] == 1:
+        employees[name]["工资"] += 1000
+        employees[name]["级别"] += 1
+    elif employees[name]["级别"] == 2:
+        employees[name]["工资"] += 2500
+    elif employees[name]["级别"] == 3:
+        employees[name]["部门"] = "管理"
+
+print(employees)
