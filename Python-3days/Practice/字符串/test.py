72709788f0d82413f1ce97918c97a811f213835f
Alioth <3434039767@qq.com>
2024-10-15 19:48:55 +0800

    ✨ feat: 新增字符串的大小写及取出特定下标区间字符等功能。

--- Python-3days/Practice/字符串/test.py
+++ Python-3days/Practice/字符串/test.py
@@ -1,5 +1,10 @@
 
 str = "I am a Python Programmer"
+print(str.upper())
+print(str.lower())
+print(str.capitalize())
+print(str.title())
+print(str[7:24])
 print(str.count("P"))
 str = str.replace(" ", "|")
 print(str)
