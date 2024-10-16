e0e319efc6535cf704a1961e018a86c9ce4e3ea7
Alioth <3434039767@qq.com>
2024-10-16 21:45:36 +0800

    ✨ feat: 提交了根据输入的数字生成列表的测试文件。

--- 牛客/Practice/python入门语法/生成数字列表/test.py
+++ 牛客/Practice/python入门语法/生成数字列表/test.py
@@ -1,4 +1,20 @@
 
 # * 1
 def func_1():
+    str = input().split()
+    num_list = []
+    for i in str:
+        num_list.append(int(i))
+    return num_list
+
+# * 2
+def func_2():
+    list = [int(i) for i in input().split()]
+    return list
+
+
+def main():
+    print(func_1())
+    print(func_2())
     
+main()
