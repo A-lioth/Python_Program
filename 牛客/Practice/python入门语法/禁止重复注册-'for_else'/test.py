be057c01b4206f9ac38406ea1d33cd277fa96c23
Alioth <3434039767@qq.com>
2024-10-20 22:00:58 +0800

    ✨ feat: 提交了关于用户名检测的两种方法及对应的测试文件。

--- 牛客/Practice/python入门语法/禁止重复注册-'for_else'/test.py
+++ 牛客/Practice/python入门语法/禁止重复注册-'for_else'/test.py
@@ -1,6 +1,7 @@
 #
 current_users = ["Niuniu", "Niumei", "GURR", "LOLO"]
 new_users = ["GurR", "Niu Ke Le", "LoLo", "Tuo Rui Chi"]
+# * 1 双重循环
 for i in new_users:
     for j in current_users:
         if i.lower() == j.lower():
@@ -12,3 +13,10 @@ for i in new_users:
             break
     else:
         print("Congratulations, the user name", i, "is available!")
+
+# * 2 if...in.../map()/lambda/list()
+for c in new_users:
+    if c.upper() not in list(map(lambda x: x.upper(), current_users)):
+        print("Congratulations, the user name", c, "is available!")
+    else:
+        print("The user name", c, "has already been registered! Please change it and try again!")
