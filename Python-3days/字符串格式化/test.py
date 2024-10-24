271c13ad6d3d6c13c139fcda7b96d46f75b425f1
Alioth <3434039767@qq.com>
2024-10-24 16:30:38 +0800

    ✨ feat: 新增使用", "和"+"连接的字符串在输出时是否添加空格的测试代码。

--- Python-3days/字符串格式化/test.py
+++ Python-3days/字符串格式化/test.py
@@ -1,7 +1,11 @@
-
+# 
 # * hello world
 name = "world"
 print("Hello, %s!" % name)
+    # * 使用"+"连接的字符串在输出时不会自动添加空格
+print("Hello, " + name + "!")
+    # * 使用","分隔的字符串在输出时会自动添加空格
+print("Hello,", name, "!")
 
 # * I love you
 v = "love"
@@ -18,3 +22,4 @@ print("%d + %d = %d" % (num1, num2, num1 + num2))
 print(f"{num1} + {num2} = {num1 + num2}")
 print("{} + {} = {}".format(num1, num2, num1 + num2))
 print(f"{num1:02d} + {num2:02d} = {num1 + num2:02d}")
+print(num1, "+", num2, "=", num1 + num2)
