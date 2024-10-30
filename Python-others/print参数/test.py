80fceebc35327817d79cda5c13ec409efb538240
Alioth <20231797@stu.nmu.edu.cn>
2024-10-30 22:43:24 +0800

    ✨ feat: 新增'print()'函数的参数sep和end的测试代码。

---
+++ Python-others/print参数/test.py
@@ -0,0 +1,9 @@
+# 
+# * sep: 指定分隔符，默认是空格
+print("Hello", "World", "Python", sep="-")
+
+# * end: 指定结尾符，默认是换行符
+print("Hello", "World", "Python", end="\n")
+
+# * 综合
+print("Hello", "World", "Python", sep=", ", end="!")
