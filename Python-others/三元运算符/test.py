41fe5bc89d86a71884d113089394c73ced9368bc
Alioth <20231797@stu.nmu.edu.cn>
2024-10-31 21:16:08 +0800

    ✨ feat: 增加使用列表推导式嵌套三元运算符和’for‘循环实现输出字符串的两种格式的测试代码。

--- Python-others/三元运算符/test.py
+++ Python-others/三元运算符/test.py
@@ -1,12 +1,36 @@
 # 
 # * 比大小
     # * 两个数
-a, b = map(int, input().split())
-max = a if a > b else b
-min = a if a < b else b
-print(max, min)
+# a, b = map(int, input().split())
+# max = a if a > b else b
+# min = a if a < b else b
+# print(max, min)
     # * 多个数
-a, b, c = map(int, input().split())
-max = a if a> b else (b if b > c else c)
-min = a if a < b else (b if b < c else c)
-print(max, min)
+# a, b, c = map(int, input().split())
+# max = a if a> b else (b if b > c else c)
+# min = a if a < b else (b if b < c else c)
+# print(max, min)
+
+# * 输出字符串
+name = ['alice', 'bob', 'tom']
+    # * 每行输出一个字符串
+boss = ['tim', 'tom']
+[
+    print
+    (
+        '我是' + i 
+        if i in boss 
+        else '我不是' + i
+    ) 
+    for i in name
+]
+    # * 一行输出所有字符串
+print
+(
+    [
+        '我是' + i 
+        if i in boss 
+        else '我不是' + i 
+        for i in name
+    ]
+)
